a541b9519b7dc77e6c91c6e319848073c16c965f
apoorvt4 <apoorvtiwari292@gmail.com>
2024-08-10 03:12:30 +0530

    commet

--- Main.py
+++ Main.py
@@ -1,358 +1,358 @@
-# def swaplist(newlist):
-#     size = len(newlist)
+def swaplist(newlist):
+    size = len(newlist)
 
-#     temp = newlist[0]
-#     newlist[0] = newlist[size - 1]
+    temp = newlist[0]
+    newlist[0] = newlist[size - 1]
 
-#     newlist[size - 1] = temp
+    newlist[size - 1] = temp
 
-#     return newlist
+    return newlist
 
 
 # newlist = [12, 35, 9, 56, 24]
-            #  [0, 1, 2, 3, 4]
+#              [0, 1, 2, 3, 4]
 # print(swaplist(newlist))
 
-# def first_last_name(name):
-#     name[1], name[3] = name[3], name[1]
-#     return name
+def first_last_name(name):
+    name[1], name[3] = name[3], name[1]
+    return name
 
-# name = [12, 35, 9, 56, 24]
-# print(first_last_name(name))
+name = [12, 35, 9, 56, 24]
+print(first_last_name(name))
 
 
 
-# def number(num):
-#     get = num[-1], num[0]
-#     num[0], num[-1] = get
-#     return num
+def number(num):
+    get = num[-1], num[0]
+    num[0], num[-1] = get
+    return num
 
-# num = [12, 35, 9, 56, 24]
-# print(number(num))
+num = [12, 35, 9, 56, 24]
+print(number(num))
     
 
 
-# def number(num):
-#     start, *middle, end = num
-#     num = [end, *middle, start]
-#     return num
+def number(num):
+    start, *middle, end = num
+    num = [end, *middle, start]
+    return num
 
-# num = input("Enter The Number \n")
-# print(number(num))
+num = input("Enter The Number \n")
+print(number(num))
 
-# class Solution:
+class Solution:
     
-#     # Note that the size of the array is n-1
-#     def missingNumber(self, n, arr):
-#         self.n = n
-#         self.arr = arr
+    # Note that the size of the array is n-1
+    def missingNumber(self, n, arr):
+        self.n = n
+        self.arr = arr
 
-#     def array(missingNumber):
-#         arr = [1, 2, 3, 5]
-#         n = 4
-#         return n, arr
+    def array(missingNumber):
+        arr = [1, 2, 3, 5]
+        n = 4
+        return n, arr
     
 
-# a = Solution()
-# Solution.array(4)
-# print(4)
+a = Solution()
+Solution.array(4)
+print(4)
 
         
-# class Solution:
+class Solution:
     
-#     # Note that the size of the array is n-1
-#     def missingNumber(self, n, arr):
-#         sum=n*(n+1)//2
-#         for i in range(n-1):
-#             sum-=arr[i]
-#         return sum
+    # Note that the size of the array is n-1
+    def missingNumber(self, n, arr):
+        sum=n*(n+1)//2
+        for i in range(n-1):
+            sum-=arr[i]
+        return sum
 
-# class solution:
-#     def missingNumber(self, n, arr):
-#         total_sum = n*(n + 1) // 2
-#         array_sum = sum(arr)
+class solution:
+    def missingNumber(self, n, arr):
+        total_sum = n*(n + 1) // 2
+        array_sum = sum(arr)
 
-#         missing_element = total_sum - array_sum
-#         return missing_element
+        missing_element = total_sum - array_sum
+        return missing_element
     
-# solution = solution()
-# arr = [1, 2, 3, 5]
-# n = 5
-# print(solution.missingNumber(n, arr))
+solution = solution()
+arr = [1, 2, 3, 5]
+n = 5
+print(solution.missingNumber(n, arr))
 
-# def find_missing_element(arr, n):
-#     # Calculate the sum of the first n natural numbers
-#     total_sum = n * (n + 1) // 2
+def find_missing_element(arr, n):
+    # Calculate the sum of the first n natural numbers
+    total_sum = n * (n + 1) // 2
     
-#     # Calculate the sum of the elements in the array
-#     array_sum = sum(arr)
+    # Calculate the sum of the elements in the array
+    array_sum = sum(arr)
     
-#     # The missing element is the difference between the total sum and the array sum
-#     missing_element = total_sum - array_sum
+    # The missing element is the difference between the total sum and the array sum
+    missing_element = total_sum - array_sum
     
-#     return missing_element
+    return missing_element
 
-# # Example usage
-# arr = [1, 2, 4, 5, 6]
-# n = 6
-# print(find_missing_element(arr, n))  # Output will be 3
-
-
-# class Solution:
-#     def missingNumber(self, n, arr):
-#         total_sum = n * (n + 1) // 2
-#         array_sum = sum(arr)
-#         missing_element = total_sum - array_sum
-#         return missing_element
-
-# # Creating an instance of the Solution class
-# solution = Solution()
-
-# # Example usage
-# arr = [1, 2, 3, 5]
-# n = 5
-# print(solution.missingNumber(n, arr))  # Output will be 4
+# Example usage
+arr = [1, 2, 4, 5, 6]
+n = 6
+print(find_missing_element(arr, n))  # Output will be 3
 
 
+class Solution:
+    def missingNumber(self, n, arr):
+        total_sum = n * (n + 1) // 2
+        array_sum = sum(arr)
+        missing_element = total_sum - array_sum
+        return missing_element
 
-# class solution:
-#     def missingNumber(self, n, arr):
-#         total_sum = n*(n + 1) // 2
-#         array_sum = sum(arr)
+# Creating an instance of the Solution class
+solution = Solution()
 
-#         missing_element = total_sum - array_sum
-#         return missing_element
+# Example usage
+arr = [1, 2, 3, 5]
+n = 5
+print(solution.missingNumber(n, arr))  # Output will be 4
+
+
+
+class solution:
+    def missingNumber(self, n, arr):
+        total_sum = n*(n + 1) // 2
+        array_sum = sum(arr)
+
+        missing_element = total_sum - array_sum
+        return missing_element
     
-# solution = solution()
-# arr = [1, 2, 3, 5]
-# n = 5
-# print(solution.missingNumber(n, arr))
+solution = solution()
+arr = [1, 2, 3, 5]
+n = 5
+print(solution.missingNumber(n, arr))
 
-# x = 5
-# y = "john"
+x = 5
+y = "john"
 
-# print(x, y)
+print(x, y)
 
-# x = 5
-# y = [12, 55]
-# z = 4.5
-# zx = "Apoorv"
+x = 5
+y = [12, 55]
+z = 4.5
+zx = "Apoorv"
 
-# print(type(x)), print(type(y)), print(type(z)), print(type(zx))
-
-
-# __var__pk = 55
-# print(__var__pk)
-
-# x, y, z = "Apple", "Banana", 55.2
-
-# print(x), print(y), print(z)
-
-# x = y = z = "Orange"
-# print(x)
-
-# fruits = ["apple", "banana", "cherry"]
-# x, y, z = fruits
-# print(x)
-# print(y)
-# print(z)
-
-# fruits = ["Apoorv", "Rishi", "Somi"]
-# x, y, z = fruits
-
-# print(x)
-# print(y)
-# print(z)
+print(type(x)), print(type(y)), print(type(z)), print(type(zx))
 
 
-# x = "Apoorv"
-# y = "Tiwari"
+__var__pk = 55
+print(__var__pk)
 
-# print(f"This man called {x, y} is awesom", end='!')
+x, y, z = "Apple", "Banana", 55.2
 
-# x = "Awesom"
-# def func():
-#     print(f"Python is {x}")
+print(x), print(y), print(z)
 
-# func()
+x = y = z = "Orange"
+print(x)
 
-# class TimePass:
-#     def func(self):
-#         a = "Apoorv"
-#         print(a)
-#     def func1(self):
-#         b = "Tiwari"
-#         print(b)
-#     def func2(self):
-#         c = "Is"
-#         print(c)
-#     def func3(self):
-#         d = "Is"
-#         print(d)
-#     def func4(self):
-#         e = "Is"
-#         print(e)
-#     def func5(self):
-#         f = "Is"
-#         print(f)
-#     def func6(self):
-#         g = "Is"
-#         print(g)
-#     def func7(self):
-#         h = "Is"
-#         print(h)
-#     def func8(self):
-#         i = "Is"
-#         print(i)
-#     def func9(self):
-#         for i in range(5):
-#             print(i)
-#     def func10(self):
-#         for i in range(100):
-#                 print(i)
-#                 def func11(self):
-#                     x = 55
-#                     print(x)
-#                     def func12(self):
-#                         y = 55
-#                         print(y)
-#                         def func13(self):
-#                             e = 96
-#                             print(e)
-#                             def func14(self):
-#                                 for a in range(50):
-#                                     print(a)
-#                                     for i in range:
-#                                         print(i)
-#                                         if i >= a:
-#                                             print(a)
-#                                             def func15(self):
-#                                                 r = "Apoorv"
-#                                                 print(f"What the hell is this {r}"), print("This is how we do Maths")
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+fruits = ["Apoorv", "Rishi", "Somi"]
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
+
+
+x = "Apoorv"
+y = "Tiwari"
+
+print(f"This man called {x, y} is awesom", end='!')
+
+x = "Awesom"
+def func():
+    print(f"Python is {x}")
+
+func()
+
+class TimePass:
+    def func(self):
+        a = "Apoorv"
+        print(a)
+    def func1(self):
+        b = "Tiwari"
+        print(b)
+    def func2(self):
+        c = "Is"
+        print(c)
+    def func3(self):
+        d = "Is"
+        print(d)
+    def func4(self):
+        e = "Is"
+        print(e)
+    def func5(self):
+        f = "Is"
+        print(f)
+    def func6(self):
+        g = "Is"
+        print(g)
+    def func7(self):
+        h = "Is"
+        print(h)
+    def func8(self):
+        i = "Is"
+        print(i)
+    def func9(self):
+        for i in range(5):
+            print(i)
+    def func10(self):
+        for i in range(100):
+                print(i)
+                def func11(self):
+                    x = 55
+                    print(x)
+                    def func12(self):
+                        y = 55
+                        print(y)
+                        def func13(self):
+                            e = 96
+                            print(e)
+                            def func14(self):
+                                for a in range(50):
+                                    print(a)
+                                    for i in range:
+                                        print(i)
+                                        if i >= a:
+                                            print(a)
+                                            def func15(self):
+                                                r = "Apoorv"
+                                                print(f"What the hell is this {r}"), print("This is how we do Maths")
                                                 
 
 
-# z = TimePass()
-# z.func()
-# z.func1()
-# z.func2()
-# z.func3()
-# z.func4()
-# z.func5()
-# z.func6()
-# z.func7()
-# z.func8()
-# z.func9()
-# z.func10()
+z = TimePass()
+z.func()
+z.func1()
+z.func2()
+z.func3()
+z.func4()
+z.func5()
+z.func6()
+z.func7()
+z.func8()
+z.func9()
+z.func10()
 
 
-# def myfunc():
-#   global x
-#   x = "fantastic"
+def myfunc():
+  global x
+  x = "fantastic"
 
-# myfunc()
+myfunc()
 
-# print("Python is " + x)
-
-
-# import random
-
-# print(random.randrange(50))
-
-# a = '''
-# This is me nd this my python course here are we gonna say thata i am a computer gkeek and there is a very decent amount of peoplrliving in this city that are very awesom to thereculture in the city and we gonna use there sak of cake of gothe r but are we not gooona say that re we ae thre one who are true value of the are who peoppllr
+print("Python is " + x)
 
 
+import random
 
-# '''
+print(random.randrange(50))
 
-
-# print(a)
-
-
-# a = "Hello World"
-# print(a[0])
-
-# def number(name):
-#     first, *middle, last = name
-#     name = [last, *middle, first]
-#     return name
+a = '''
+This is me nd this my python course here are we gonna say thata i am a computer gkeek and there is a very decent amount of peoplrliving in this city that are very awesom to thereculture in the city and we gonna use there sak of cake of gothe r but are we not gooona say that re we ae thre one who are true value of the are who peoppllr
 
 
 
-# name = [12, 5, 89, 78, 55]
-# print(number(name))
+'''
 
 
-# for x in "banana":
-#     print(x)
+print(a)
 
-# for x in "banana":
-#   print(x)
+
+a = "Hello World"
+print(a[0])
+
+def number(name):
+    first, *middle, last = name
+    name = [last, *middle, first]
+    return name
+
+
+
+name = [12, 5, 89, 78, 55]
+print(number(name))
+
+
+for x in "banana":
+    print(x)
+
+for x in "banana":
+  print(x)
 
 # for x in banan:;
 # printg9x
 
-# for x in 'banana':
-#     print(x)
+for x in 'banana':
+    print(x)
 
-# a = "Hello World!"
-# print(len(a))
-
-
-# text = "The best thing in life is free"
-
-# if "on" in text:
-#     print("yes, it is present")
-# else:
-#     print("No")
+a = "Hello World!"
+print(len(a))
 
 
+text = "The best thing in life is free"
 
-# a = [12, 55, 89, 78]
-# b = int(input("Enter your number\n"))
-# c = int(input("Enter your number\n"))
-# if b in a or c in a:
-#    print("yes")
-# else:
-#    print("No")
+if "on" in text:
+    print("yes, it is present")
+else:
+    print("No")
 
 
 
-# b = "hello, World!"
-# print(b[2:5])
+a = [12, 55, 89, 78]
+b = int(input("Enter your number\n"))
+c = int(input("Enter your number\n"))
+if b in a or c in a:
+   print("yes")
+else:
+   print("No")
 
-# b = "Hello, world!"
-# print(b[:])
 
-# a = "Hello, World!"
-# print(a.lower())
 
-# a = "Hello World!"
-# print(a.strip())
+b = "hello, World!"
+print(b[2:5])
 
-# a =  "Hello Wrold!"
-# print(a.replace("Hello", "Jisma"))
+b = "Hello, world!"
+print(b[:])
 
-# a = "Hello World!"
-# print(a.split(","))
-# a = "Hello"
-# b = "World"
-# c = a + " " + b
-# print(c)
+a = "Hello, World!"
+print(a.lower())
 
-# a = 21
-# print("My name is Apoorv Tiwari and my age is", a)
+a = "Hello World!"
+print(a.strip())
 
-# price = 599
-# text = f"The price is {price:.2f}$"
-# print(text)
+a =  "Hello Wrold!"
+print(a.replace("Hello", "Jisma"))
 
-# text = f"The price is {55 * 89:.2f} dollar"
-# print(text)
+a = "Hello World!"
+print(a.split(","))
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
 
-# txt = "We are the so-called \"Vikings\" from the north."
-# print(txt)
+a = 21
+print("My name is Apoorv Tiwari and my age is", a)
+
+price = 599
+text = f"The price is {price:.2f}$"
+print(text)
+
+text = f"The price is {55 * 89:.2f} dollar"
+print(text)
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
 
 a = "555"
 print(a.isdecimal())
